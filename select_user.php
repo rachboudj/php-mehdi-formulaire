b70564a6d6d06788b033412af42225524cc553a1
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-06 14:17:11 +0200

    ajout d'un tableau où l'on peut modifier et supprimer des users

--- select_user.php
+++ select_user.php
@@ -1,1 +1,38 @@
-
+<?php
+require('includes/pdo.php');
+require('includes/function.php');
+$title = "Contact";
+// Get all beers
+$sql = "SELECT * FROM users ORDER BY created_at DESC";
+$query = $pdo->prepare($sql);
+$query->execute();
+// fetchAll, fetch , fetchColumn
+$users = $query->fetchAll();
+debug($users);
+?>
+<h1>Les utilisateurs</h1>
+<table>
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>nom</th>
+            <th>prenom</th>
+            <th>email</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($users as $user) { ?>
+            <tr>
+                <td><?= $user['id']; ?></td>
+                <td><?= $user['nom']; ?></td>
+                <td><?= $user['prenom']; ?></td>
+                <td><?= $user['email']; ?></td>
+                <td><a href="edit_user.php?id=<? $user['id'] ?>">Éditer</a></td>
+                <td><a href="supp.php?id=<? $user['id'] ?>">Supprimer</a></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+<?php // include('inc/footer.php');
