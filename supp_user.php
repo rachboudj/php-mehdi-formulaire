c960e34ba541390d57750e5d14f639e4bd08d189
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-07-06 16:06:17 +0200

    ajout d'une fonction pour supprimer un utilisateur

--- supp_user.php
+++ supp_user.php
@@ -2,3 +2,15 @@
 require('./includes/pdo.php');
 
 // DELETE FROM users WHERE id=
+if(!empty($_GET['id']) && is_numeric($_GET['id'])) 
+{
+    $id = $_GET['id'];
+    $sql = "DELETE FROM users WHERE id = :id";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':id',$id, PDO::PARAM_INT);
+    $query->execute();
+    // $users = $query->fetch();
+    header('Location: index.php');
+} else {
+    die("Aucun parametre n'est passé à la page de suppression");
+}
